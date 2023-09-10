aa4de7d57755a83084445b204daf10c22315bda3
dmitry <dimabogacan@gmail.com>
2023-09-10 20:04:52 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,27 @@
+// Animation func
+
+function onEntry(entry) {
+    entry.forEach(change => {
+        if (change.isIntersecting) {
+                  change.target.classList.add('element-show');
+
+        } else {
+                  change.target.classList.remove('element-show');
+
+        }
+
+    });
+
+}
+
+let options = {
+      threshold: [0.5]
+
+};
+let observer = new IntersectionObserver(onEntry, options);
+let elements = document.querySelectorAll('.element_animation');
+
+for (let elm of elements) {
+      observer.observe(elm);
+
+}
